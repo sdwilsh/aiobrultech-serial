c2df761bfcbd817c3feaf25fe3fda319f9c5cb57
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2023-12-01 15:50:06 -0800

    add renovate validation

---
+++ earthly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run \
+    --privileged \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --rm \
+    -t \
+    -v "$(pwd)":/workspace \
+    -v earthly-tmp:/tmp/earthly:rw \
+    earthly/earthly:v0.7.22 \
+    "$@"
